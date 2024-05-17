1dafd82f5e89c2110dd5e27b443d0dc072b31288
KR_Hound <51309303+KRhound@users.noreply.github.com>
2024-05-18 03:52:24 +0900

    Calendar backend functionality completed

--- app.py
+++ app.py
@@ -1,37 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
 import calendar as cal
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
-
-
 
 # 년, 월 선택 기능
 @app.route('/')
 def index():
-    current_year = 2024
+    current_year = datetime.now().year
     return render_template('index.html', current_year=current_year)
 
 # calendar 기눙
-@app.route('/calendar', methods=['POST'])
+@app.route('/calendar', methods=['GET', 'POST'])
 def calendar():
-    year = int(request.form['year'])
-    month = int(request.form['month'])
-    first_day, num_days = get_days_in_month(year, month)
-    conn = sqlite3.connect('web.db')
-    cursor = conn.cursor()
-    select_query = """
-    SELECT year, month, day
-    FROM boards WHERE year = ? AND month = ? status = 1
-    """
-    cursor.execute(select_query, (year, month))
-    events = cursor.fetchall()
-    return render_template('calendar.html', year=year, month=month, first_day=first_day, num_days=num_days, events=events)
+    if request.method == 'GET':
+        now = datetime.now()
+        year = now.year
+        month = now.month
+        first_day, num_days = get_days_in_month(year, month)
+        conn = sqlite3.connect('web.db')
+        cursor = conn.cursor()
+        select_query = """
+        SELECT day, event, description
+        FROM events WHERE year = ? AND month = ? AND status = 1
+        """
+        cursor.execute(select_query, (year, month))
+        events = cursor.fetchall()
+        if events:
+            return render_template('calendar.html', year=year, month=month, first_day=first_day, num_days=num_days, events=events)
+        else:
+            return render_template('calendar.html', year=year, month=month, first_day=first_day, num_days=num_days)
+    elif request.method == 'POST':
+        year = int(request.form['year'])
+        month = int(request.form['month'])
+        first_day, num_days = get_days_in_month(year, month)
+        conn = sqlite3.connect('web.db')
+        cursor = conn.cursor()
+        select_query = """
+        SELECT day, event, description
+        FROM events WHERE year = ? AND month = ? AND status = 1
+        """
+        cursor.execute(select_query, (year, month))
+        events = cursor.fetchall()
+        if events:
+            return render_template('calendar.html', year=year, month=month, first_day=first_day, num_days=num_days, events=events)
+        else:
+            return render_template('calendar.html', year=year, month=month, first_day=first_day, num_days=num_days)
 
 # 학사일정 추가 기능
 @app.route('/add', methods=['GET', 'POST'])
 def add_event():
-    current_year = 2024
+    current_year = datetime.now().year
     if request.method == 'POST':
         year = int(request.form['year'])
         month = int(request.form['month'])
